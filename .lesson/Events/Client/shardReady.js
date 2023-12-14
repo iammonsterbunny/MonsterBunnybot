459154334c1e7d01e4e6d86105f60fca10fb02fe
Monster Bunny <141579057+Monster-Bunny@users.noreply.github.com>
2023-12-14 14:30:20 +0530

    ᴍᴏɴsᴛᴇʀ ʙᴜɴɴʏ ʙᴏᴛ ♫

---
+++ .lesson/Events/Client/shardReady.js
@@ -0,0 +1,15 @@
+/**
+ * @param {import("../Struct/Client")} client
+ */
+export default async (shardId) => {
+    console.log(`ᴍᴏɴsᴛᴇʀ ʙᴜɴɴʏ ʙᴏᴛ ♫ Spawnned Shard Id - ${shardId}`)
+}
+
+
+
+
+
+
+
+
+
